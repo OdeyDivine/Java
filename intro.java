e4abaa813781342a6137128ce6863f5dbba0e9b9
DM Tech <odeydivine16@gmail.com>
2026-04-20 01:39:56 +0100

    first commit for my learning

---
+++ intro.java
@@ -0,0 +1,24 @@
+public class intro{
+    public static void main(String[] args){
+       System.out.println("Hello world");
+
+       String name = "Divine";
+       int num = 5;
+       double twice = 6.9;
+       char first_initial = 'D';
+       boolean isHome = false;
+
+       System.out.println(name);
+       System.out.println(num);
+       System.out.println(twice);
+       System.out.println(first_initial);
+       System.out.println(isHome);
+
+       if(name.equals("Divine")){
+        System.out.println("Welcome buddy to our community");
+       }else{
+        System.out.println("You are not recognised here");
+       }
+
+    }
+}
